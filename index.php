9dcf78ec357ee7ed9f885c57e239ac35c1a23263
MonicaS17 <monica170305@gmail.com>
2025-04-20 11:43:30 -0500

    Se agregó la fecha en los footers
    Se agregó la fecha en los footers del index y de los problemas

--- index.php
+++ index.php
@@ -32,7 +32,10 @@
     </div>
 
     <footer>
-        © 2025 Grupo: 1GS131 | Realizado por Liliana Coronado y Mónica Serrano
+        © 2025 Grupo: 1GS131 | Realizado por Liliana Coronado y Mónica Serrano <br>
+        <?php 
+        echo "Fecha: " . date("d")." / ".date("m"). " / ".date("Y");
+        ?>
     </footer>
 </div>
 </body>
